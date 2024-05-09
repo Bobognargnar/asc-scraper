961d30dfef9ea64388099c8b1e7b939d76eec5e7
Bobognargnar <114653007+Bobognargnar@users.noreply.github.com>
2024-05-09 13:15:49 +0200

    fix: Removed unused code

--- scraper.py
+++ scraper.py
@@ -4,17 +4,6 @@ import argparse
 import os
 from datetime import datetime as dt
 
-def load_json_file(file_path):
-    try:
-        with open(file_path, 'r') as file:
-            data = json.load(file)
-        return data
-    except FileNotFoundError:
-        print(f"File '{file_path}' not found.")
-        return None
-    except json.JSONDecodeError:
-        print(f"Error decoding JSON in file '{file_path}'.")
-        return None
 
 class ScraperASC():
     def __init__(self,usr,pwd,dest) -> None:
